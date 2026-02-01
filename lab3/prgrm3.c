7d618341478b40bf767da92d9bca013f67de5b04
DEVIDUTT MISHRA <imca.25mimi97@silicon.ac.in>
2026-02-01 20:19:20 +0530

    Update prgrm3.c

--- lab3/prgrm3.c
+++ lab3/prgrm3.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+
+void capitalize(char s[]) {
+    int i;
+    for (i = 0; s[i] != '\0'; i++) {
+        if (i == 0 || s[i - 1] == ' ') {
+            if (s[i] >= 'a' && s[i] <= 'z') {
+                s[i] = s[i] - 32;
+            }
+        }
+    }
+}
+
+int main() {
+    char s[100];
+
+    printf("Enter a sentence: ");
+    gets(s); 
+
+    capitalize(s);
+
+    printf("Your sentence with all first letters capitalized:\n%s", s);
+
+    return 0;
+}
+
