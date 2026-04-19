068b13f31e054b2e6ca53fe230a72a20232b8274
DEVIDUTT MISHRA <imca.25mimi97@silicon.ac.in>
2026-04-19 19:03:58 +0530

    Delete MACRO/exp12/1.c

--- MACRO/exp12/1.c
+++
@@ -1,8 +0,0 @@
-/*Write a program using #define to declare constants and use them in arithmetic 
-operations.*/
-
-#define ADD(a,b) (a+b)
-
-void main(){
-	printf("Addition of two numbers-%d",ADD(2,2));
-}
