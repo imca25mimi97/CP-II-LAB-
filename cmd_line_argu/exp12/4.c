6d23cf5bc6f42628b2ce1c788e1b207d4fd693d4
DEVIDUTT MISHRA <imca.25mimi97@silicon.ac.in>
2026-05-03 08:19:29 +0530

    Update 4.c

---
+++ cmd_line_argu/exp12/4.c
@@ -0,0 +1,6 @@
+//Create a program to count the number of command-line arguments passed.
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+    printf("Number of command-line arguments: %d\n", argc);
+    return 0;
+}
