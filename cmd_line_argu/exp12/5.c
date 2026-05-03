3d9d3e71619382c0fe1925e42da1d42b037fde97
DEVIDUTT MISHRA <imca.25mimi97@silicon.ac.in>
2026-05-03 08:18:36 +0530

    Add files via upload

---
+++ cmd_line_argu/exp12/5.c
@@ -0,0 +1,7 @@
+/*Write a program that accepts a string as a command-line argument and converts
+it to uppercase.*/
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+    printf("%s\n", argv[1]);
+    return 0;
+}
